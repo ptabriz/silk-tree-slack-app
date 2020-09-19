e810e32f73a11050a6e9040d36a95eb45e37e80d
Payam <ptabriz@ncsu.edu>
2020-09-18 22:56:32 -0700

    Update server.py

--- server.py
+++ server.py
@@ -10,10 +10,9 @@ from slack import WebClient
 # from slackclient import SlackClient
 
 app = Flask(__name__, template_folder='')
-
 # Set the token from the secret environment variables.
 client = WebClient(token=os.environ.get('TOKEN'))
-print (client)
+
 @app.route('/', methods=['GET'])
 def main():
   return Response("To get started, remix this project and check out the README file!")
@@ -22,27 +21,66 @@ def main():
 @app.route('/interactive', methods=['POST'])
 def interactive():
     payload = json.loads(request.form["payload"])
-    # Extra Credit: Uncomment out this section
-    channelID = "C01BA6R20UR"
-    getall = payload["view"]["state"]["values"]
-    number= getall["number"]["numberAction"]["value"]
-    date = getall["dateBlock"]["dateAction"]["selected_date"]
-    hour= getall["hourBlock"]["hourAction"]["selected_option"]["text"]["text"]
-    print (payload)
-    d = datetime.datetime(int(date.split("-")[0]),int(date.split("-")[1]),int(date.split("-")[2]))
-    day = d.strftime('%A')
-    user = payload["user"]["id"]
 
-    if "commentBlock" in getall.keys():
-      comments =  getall["commentBlock"]["commentAction"]["value"]
-      finalComments= "They also mentiond that" + "" + comments
-    else:
-      finalComments = ""
+    type = payload["view"]["title"]["text"]
 
-    user_text= "Hello <!channel>,\n \n <@{6}> Is having {0} guests in the backyard on {1}, {2} {3} in the {4}. {5}".format(number,day, d.strftime("%B"), d.day, hour.split("(")[0], finalComments, user)
-    # user_text = comments
-    client.chat_postMessage(channel=channelID, text=user_text)
+    if type == "Fitness scheduler":
+        channelID = "C01BA6R20UR"
+        user_text = fitnessOutput(payload)
+        client.chat_postMessage(channel=channelID, link_names=1, text=user_text)
+
+    elif type == "Backyard scheduler":
+        channelID = "C01BA6R20UR"
+        user_text = backyardOutput(payload)
+        client.chat_postMessage(channel=channelID, link_names=1, text=user_text)
+
     return Response()
+
+def fitnessOutput(payload):
+  getall = payload["view"]["state"]["values"]
+
+  hour= getall["hourBlock"]["hourAction"]["value"]
+  type= getall["typeBlock"]["typeAction"]["selected_option"]["text"]["text"]
+
+  if "coachBlock" in getall.keys():
+    coach="with coach "+ getall["coachBlock"]["coachAction"]["selected_option"]["text"]["text"]
+  else:
+    coach=""
+
+  if "durationBlock" in getall.keys():
+    duration = getall["durationBlock"]["durationAction"]["value"] + " "
+  else:
+    duration = ""
+
+  if "locationBlock" in getall.keys():
+    location = getall["locationBlock"]["locationAction"]["value"] + " "
+  else:
+    location = ""
+
+  text = "Hello <!channel>, who is down for {0}{1}{2} {3}at {4} today? ".format(duration,type,coach,location,hour)
+  return text
+
+
+def backyardOutput(payload):
+  getall = payload["view"]["state"]["values"]
+
+  number= getall["number"]["numberAction"]["value"]
+  date = getall["dateBlock"]["dateAction"]["selected_date"]
+  hour= getall["hourBlock"]["hourAction"]["selected_option"]["text"]["text"]
+  print (payload)
+  d = datetime.datetime(int(date.split("-")[0]),int(date.split("-")[1]),int(date.split("-")[2]))
+  day = d.strftime('%A')
+  user = payload["user"]["id"]
+
+  if "commentBlock" in getall.keys():
+    comments =  getall["commentBlock"]["commentAction"]["value"]
+    finalComments= "They also mentiond that" + "" + comments
+  else:
+    finalComments = ""
+
+  user_text= "Hello <!channel>,\n \n <@{6}> Is having {0} guests in the backyard on {1}, {2} {3} in the {4}. {5}".format(number,day, d.strftime("%B"), d.day, hour.split("(")[0], finalComments, user)
+
+  return text
 
 # Step 5: Payload is sent to this endpoint, we extract the `trigger_id` and call views.open
 @app.route('/slashcommand', methods=['GET', 'POST'])
@@ -50,6 +88,14 @@ def slashcommand():
     with open("modal.txt") as modalfile:
         client.views_open(trigger_id=request.form["trigger_id"], view=json.load(modalfile))
     return Response()
+
+
+@app.route('/fitcommand', methods=['GET', 'POST'])
+def fitcommand():
+    with open("fitness.txt") as fitfile:
+        client.views_open(trigger_id=request.form["trigger_id"], view=json.load(fitfile))
+    return Response()
+
 
 if __name__ == '__main__':
     app.run()
